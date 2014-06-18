8deca540dd18bfc38b751e45c2e7f87d74d83a0c
Mohamed Tarek Ismail <blueice123@gmail.com>
2014-06-18 18:23:10 +0300

    Added TCP Client for Testing

---
+++ TCP Client/TCP Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Client
+{
+    public class Client
+    {
+        static Socket tcpSocket;
+        static Thread SendThread, ReceiveThread;
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Connecting.....");
+
+                tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                tcpSocket.Connect("192.168.1.6", 14);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+                SendThread = new Thread(new ThreadStart(SendFunction));
+                SendThread.Start();
+                ReceiveThread = new Thread(new ThreadStart(ReceiveFunction));
+                ReceiveThread.Start();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void SendFunction()
+        {
+            byte[] Data = new byte[1024];
+            while (true)
+            {
+                Console.Write("Enter the string to be transmitted : ");
+                String str = Console.ReadLine();
+                Data = Encoding.ASCII.GetBytes(str);
+
+                Console.WriteLine("Transmitting.....");
+                tcpSocket.Send(Data);
+            }
+        }
+
+        static void ReceiveFunction()
+        {
+            byte[] ReceivedData = new byte[1024];
+            tcpSocket.Receive(ReceivedData);
+            Console.WriteLine(Encoding.ASCII.GetString(ReceivedData));
+        }
+
+    }
+}
