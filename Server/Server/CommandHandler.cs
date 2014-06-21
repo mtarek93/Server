ff4ad9cfddf8a083530a32b4d71ddb5d053a9f0c
Mohamed Tarek Ismail <blueice123@gmail.com>
2014-06-22 00:54:59 +0300

    Added check for null or empty

--- Server/Server/CommandHandler.cs
+++ Server/Server/CommandHandler.cs
@@ -100,13 +100,16 @@ namespace CommandHandler
                 {
                     Command = Command.Remove(Command.Length - 1);           //Remove delimiter
                     string[] SplittedCommand = Command.Split(',');
-                    
-                    Cmd.SourceID = BitConverter.ToUInt16(Encoding.ASCII.GetBytes(SplittedCommand[1]), 0);
-                    Cmd.DestinationID = BitConverter.ToUInt16(Encoding.ASCII.GetBytes(SplittedCommand[2]), 0);
-                    Cmd.Action_State = BitConverter.ToUInt16(Encoding.ASCII.GetBytes(SplittedCommand[3]), 0);
+
+                    if (!String.IsNullOrEmpty(SplittedCommand[1]))
+                        Cmd.SourceID = BitConverter.ToUInt16(Encoding.ASCII.GetBytes(SplittedCommand[1]), 0);
+                    if (!String.IsNullOrEmpty(SplittedCommand[2]))
+                        Cmd.DestinationID = BitConverter.ToUInt16(Encoding.ASCII.GetBytes(SplittedCommand[2]), 0);
+                    if (!String.IsNullOrEmpty(SplittedCommand[3]))
+                        Cmd.Action_State = BitConverter.ToUInt16(Encoding.ASCII.GetBytes(SplittedCommand[3]), 0);
                     Cmd.UserName = SplittedCommand[4];
                     Cmd.Password = SplittedCommand[5];
-                    
+
                     return Cmd;
                 }
             }
