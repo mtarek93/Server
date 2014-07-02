aaa82c82b1ddf3b08a2bd6d541753cae83c47686
michael ibrahim <mmibrahim@aucegypt.edu>
2014-07-02 07:16:22 -0700

    test :)

---
+++ Multiple_Send_test/Multiple_Send_test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//using Database;
+//using CommandHandler;
+using ServerTools;
+
+namespace Server
+{
+    class Server
+    {
+        public static void Main()
+        {
+            const int Port = 14;
+            IPAddress ipAd = Tools.GetMyIPAddress();
+            //List<Socket> SocketList = new List<Socket>();
+
+            /* Initializes the Listener */
+            TcpListener Server = new TcpListener(ipAd, Port);
+            Console.WriteLine("The local End point is  :" + Server.LocalEndpoint);
+            while (true)
+            {
+                try
+                {
+                    /* Start Listening at the specified port */
+                    Server.Start();
+                    Socket S = Server.AcceptSocket();
+                    Thread T = new Thread(new ParameterizedThreadStart(Tools.Send1));
+                    T.Start(S);
+                    Thread T2 = new Thread(new ParameterizedThreadStart(Tools.Send2));
+                    T2.Start(S);
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Server.main: " + e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Multiple_Send_test/Multiple_Send_test/Tools.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerTools
+{
+    class Tools
+    {
+        public static void Send1(object _Socket)
+        {
+            string D;
+            D = "Message Sent !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: 1";
+            byte[] Data = StringToByteArray(D);
+            Socket S = _Socket as Socket;
+            while (true)
+            {
+                Console.WriteLine("Sending Message: 1" );
+                S.Send(Data);
+                Thread.Sleep(50);
+            }
+        }
+        public static void Send2(object _Socket)
+        {
+            string D;
+            D = "Message Sent !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: 2";
+            byte[] Data = StringToByteArray(D);
+            Socket S = _Socket as Socket;
+            while (true)
+            {
+                Console.WriteLine("Sending Message: 2" );
+                S.Send(Data);
+                Thread.Sleep(25);
+            }
+
+        }
+        public static byte[] StringToByteArray(string data)
+        {
+            return Encoding.GetEncoding(437).GetBytes(data);      
+        } 
+        public static IPAddress GetMyIPAddress()
+        {
+            return Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+        }
+    }
+}
