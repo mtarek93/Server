4e7b944ad3cd65f1c4a3fbb56da8fe9cbc335ddf
Mohamed Tarek Ismail <blueice123@gmail.com>
2014-07-02 15:12:17 +0300

    Revert "Database thread safety"
    This reverts commit 053bb8af54651663cbb919481d7bd16e3b636c87.

--- Server/Server/Database.cs
+++ Server/Server/Database.cs
@@ -17,270 +17,241 @@ namespace Database
     class DatabaseHandler
     {
         public static string ConnectionString, LoginTable = "LoginTable", UsersTable = "UsersTable", DevicesTable = "DevicesTable", IDTable = "IDTable";
-        static object DatabaseLock = new object();
-
         public static void AddUserAccount(string Username, string Password)
         {
-            lock (DatabaseLock)
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    using (SqlCommand Command = new SqlCommand("INSERT INTO " + LoginTable + " (Username, Password) VALUES (@Username, @Password);", Database))
                     {
-                        Database.Open();
-                        using (SqlCommand Command = new SqlCommand("INSERT INTO " + LoginTable + " (Username, Password) VALUES (@Username, @Password);", Database))
-                        {
-                            Command.Parameters.Add(new SqlParameter("@Username", Username));
-                            Command.Parameters.Add(new SqlParameter("@Password", Password));
-                            Command.ExecuteNonQuery();
-                        }
-                        Database.Close();
+                        Command.Parameters.Add(new SqlParameter("@Username", Username));
+                        Command.Parameters.Add(new SqlParameter("@Password", Password));
+                        Command.ExecuteNonQuery();
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
+                    Database.Close();
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
                 }
             }
         }
         public static bool UserIsAuthenticated(string Username, string Password)
         {
-            lock (DatabaseLock)
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    string Query = "SELECT Username, Password FROM " + LoginTable + " WHERE Username = " + "'" + Username + "' " + "AND Password = " + "'" + Password + "';";
+
+                    using (SqlCommand Command = new SqlCommand(Query, Database))
                     {
-                        Database.Open();
-                        string Query = "SELECT Username, Password FROM " + LoginTable + " WHERE Username = " + "'" + Username + "' " + "AND Password = " + "'" + Password + "';";
-
-                        using (SqlCommand Command = new SqlCommand(Query, Database))
+                        SqlDataReader Reader = Command.ExecuteReader();
+                        if (Reader.Read())
                         {
-                            SqlDataReader Reader = Command.ExecuteReader();
-                            if (Reader.Read())
-                            {
-                                //Console.WriteLine("Authenticated!");
-                                return true;
-                            }
+                            //Console.WriteLine("Authenticated!");
+                            return true;
+                        }
 
-                            else
-                            {
-                                //Console.WriteLine("Please check your credentials and try again.");
-                                return false;
-                            }
+                        else
+                        {
+                            //Console.WriteLine("Please check your credentials and try again.");
+                            return false;
                         }
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                        return false;
-                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
                 }
             }
         }
         public static void AddNewUser(ushort ID)
         {
-            lock (DatabaseLock)
+            int Id = Convert.ToInt32(ID);
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                int Id = Convert.ToInt32(ID);
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    using (SqlCommand Command = new SqlCommand("INSERT INTO " + UsersTable + " (Id) VALUES (@Id);", Database))
                     {
-                        Database.Open();
-                        using (SqlCommand Command = new SqlCommand("INSERT INTO " + UsersTable + " (Id) VALUES (@Id);", Database))
-                        {
-                            Command.Parameters.Add(new SqlParameter("@Id", Id));
-                            Command.ExecuteNonQuery();
-                        }
-                        Database.Close();
+                        Command.Parameters.Add(new SqlParameter("@Id", Id));
+                        Command.ExecuteNonQuery();
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
+                    Database.Close();
                 }
-                AddNewID(ID);
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
+            AddNewID(ID);
         }
         public static void AddNewDevice(ushort ID, int State)
         {
-            lock (DatabaseLock)
+            int Id = Convert.ToInt32(ID);
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                int Id = Convert.ToInt32(ID);
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    using (SqlCommand Command = new SqlCommand("INSERT INTO " + DevicesTable + " (Id) VALUES (@Id);", Database))
                     {
-                        Database.Open();
-                        using (SqlCommand Command = new SqlCommand("INSERT INTO " + DevicesTable + " (Id) VALUES (@Id);", Database))
-                        {
-                            Command.Parameters.Add(new SqlParameter("@Id", Id));
-                            Command.ExecuteNonQuery();
-                        }
-                        Database.Close();
+                        Command.Parameters.Add(new SqlParameter("@Id", Id));
+                        Command.ExecuteNonQuery();
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
+                    Database.Close();
                 }
-                AddNewID(ID);
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
+            AddNewID(ID);
         }
         public static bool TryGetDevice(int ID, out Device D)
         {
-            lock (DatabaseLock)
+            int Id = Convert.ToInt32(ID);
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                int Id = Convert.ToInt32(ID);
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    string Query = "SELECT Id, State FROM " + DevicesTable + " WHERE Id = " + "'" + Id + "';";
+                    using (SqlCommand Command = new SqlCommand(Query, Database))
                     {
-                        Database.Open();
-                        string Query = "SELECT Id, State FROM " + DevicesTable + " WHERE Id = " + "'" + Id + "';";
-                        using (SqlCommand Command = new SqlCommand(Query, Database))
+                        SqlDataReader Reader = Command.ExecuteReader();
+                        if (Reader.Read())
                         {
-                            SqlDataReader Reader = Command.ExecuteReader();
-                            if (Reader.Read())
-                            {
-                                //Console.WriteLine("Device found!");
-                                D = new Device(Convert.ToUInt16(Reader.GetInt32(0)));
-                                return true;
-                            }
+                            //Console.WriteLine("Device found!");
+                            D = new Device(Convert.ToUInt16(Reader.GetInt32(0)));
+                            return true;
+                        }
 
-                            else
-                            {
-                                //Console.WriteLine("Device not registered.");
-                                D = null;
-                                return false;
-                            }
+                        else
+                        {
+                            //Console.WriteLine("Device not registered.");
+                            D = null;
+                            return false;
                         }
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                        D = null;
-                        return false;
-                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                    D = null;
+                    return false;
                 }
             }
         }
         public static bool TryGetUser(int ID, out User U)
         {
-            lock (DatabaseLock)
+            int Id = Convert.ToInt32(ID);
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                int Id = Convert.ToInt32(ID);
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    string Query = "SELECT Id FROM " + UsersTable + " WHERE Id = " + "'" + Id + "';";
+
+                    using (SqlCommand Command = new SqlCommand(Query, Database))
                     {
-                        Database.Open();
-                        string Query = "SELECT Id FROM " + UsersTable + " WHERE Id = " + "'" + Id + "';";
-
-                        using (SqlCommand Command = new SqlCommand(Query, Database))
+                        SqlDataReader Reader = Command.ExecuteReader();
+                        if (Reader.Read())
                         {
-                            SqlDataReader Reader = Command.ExecuteReader();
-                            if (Reader.Read())
-                            {
-                                //Console.WriteLine("User found!");
-                                U = new User(Convert.ToUInt16(Reader.GetInt32(0)));
-                                return true;
-                            }
+                            //Console.WriteLine("User found!");
+                            U = new User(Convert.ToUInt16(Reader.GetInt32(0)));
+                            return true;
+                        }
 
-                            else
-                            {
-                                //Console.WriteLine("User not registered.");
-                                U = null;
-                                return false;
-                            }
+                        else
+                        {
+                            //Console.WriteLine("User not registered.");
+                            U = null;
+                            return false;
                         }
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                        U = null;
-                        return false;
-                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                    U = null;
+                    return false;
                 }
             }
         }
         public static bool UsernameExists(string Username)
         {
-            lock (DatabaseLock)
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
-                    {
-                        Database.Open();
-                        string Query = "SELECT Username FROM " + LoginTable + " WHERE Username = " + "'" + Username + "';";
+                    Database.Open();
+                    string Query = "SELECT Username FROM " + LoginTable + " WHERE Username = " + "'" + Username + "';";
 
-                        using (SqlCommand Command = new SqlCommand(Query, Database))
-                        {
-                            SqlDataReader Reader = Command.ExecuteReader();
-                            if (Reader.Read())
-                                return true;
-                            else
-                                return false;
-                        }
-                    }
-                    catch (SqlException e)
+                    using (SqlCommand Command = new SqlCommand(Query, Database))
                     {
-                        Console.WriteLine(e.Message);
-                        return false;
+                        SqlDataReader Reader = Command.ExecuteReader();
+                        if (Reader.Read())
+                            return true;
+                        else
+                            return false;
                     }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
                 }
             }
         }
         public static ushort GetNumberofIDs()
         {
-            lock (DatabaseLock)
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    string Query = "SELECT COUNT (Id) FROM " + IDTable + ";";
+                    using (SqlCommand Command = new SqlCommand(Query, Database))
                     {
-                        Database.Open();
-                        string Query = "SELECT COUNT (Id) FROM " + IDTable + ";";
-                        using (SqlCommand Command = new SqlCommand(Query, Database))
-                        {
-                            SqlDataReader Reader = Command.ExecuteReader();
-                            if (Reader.Read())
-                                return Convert.ToUInt16(Reader.GetInt32(0));
-                            else
-                                return (ushort)0;
-                        }
+                        SqlDataReader Reader = Command.ExecuteReader();
+                        if (Reader.Read())
+                            return Convert.ToUInt16(Reader.GetInt32(0));
+                        else
+                            return (ushort)0;
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                        return (ushort)0;
-                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return (ushort)0;
                 }
             }
         }
         private static void AddNewID (ushort ID)
         {
-            lock (DatabaseLock)
+            using (SqlConnection Database = new SqlConnection(ConnectionString))
             {
-                using (SqlConnection Database = new SqlConnection(ConnectionString))
+                try
                 {
-                    try
+                    Database.Open();
+                    string Query = "INSERT INTO " + IDTable + " (Id) VALUES (@Id);";
+                    using (SqlCommand Command = new SqlCommand(Query, Database))
                     {
-                        Database.Open();
-                        string Query = "INSERT INTO " + IDTable + " (Id) VALUES (@Id);";
-                        using (SqlCommand Command = new SqlCommand(Query, Database))
-                        {
-                            Command.Parameters.Add(new SqlParameter("@Id", Convert.ToInt32(ID)));
-                            Command.ExecuteNonQuery();
-                        }
-                        Database.Close();
+                        Command.Parameters.Add(new SqlParameter("@Id", Convert.ToInt32(ID)));
+                        Command.ExecuteNonQuery();
                     }
-                    catch (SqlException e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
+                    Database.Close();
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
                 }
             }
         }
