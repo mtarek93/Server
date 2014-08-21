fd56c5ed37c92aeb0fa2620f2e90c99d53d0da34
Mohamed Tarek Ismail <blueice123@gmail.com>
2014-08-21 12:23:40 +0300

    Added new classes for Location
    - Room, zone, home, location

---
+++ Server/Server/LocationComponents.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clients;
+
+namespace LocationComponents
+{
+    class Room
+    {
+        public string Name { get; set; }
+        private List<Device> DeviceList = new List<Device>();
+
+        void AddDevice(Device D)
+        {
+            DeviceList.Add(D);
+        }
+
+        void RemoveDevice(Device D)
+        {
+            DeviceList.Remove(D);
+        }
+    }
+
+    class Zone
+    {
+    }
+
+    class Home
+    {
+    }
+
+    class Location
+    {
+        public int xCoordinate { get; set; }
+        public int yCoordinate { get; set; }
+        public Room locationRoom;
+
+        public Location(int x, int y)
+        {
+            xCoordinate = x;
+            yCoordinate = y;
+        }
+
+
+    }
+}
