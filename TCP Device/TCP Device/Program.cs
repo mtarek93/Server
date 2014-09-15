8420589d96eb5d2c19e9dfff4e02c757afc8e491
Michael Ibrahim <mmibrahim@aucegypt.edu>
2014-09-15 14:31:33 +0300

    nothing

--- TCP Device/TCP Device/Program.cs
+++ TCP Device/TCP Device/Program.cs
@@ -23,7 +23,7 @@ namespace TCP_Device
             {
                 Console.WriteLine("Connecting.....");
                 tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                tcpSocket.Connect("10.40.47.54", 14);
+                tcpSocket.Connect("10.211.55.3", 14);
                 Console.WriteLine("Connected");
 
                 ID = RequestName();
