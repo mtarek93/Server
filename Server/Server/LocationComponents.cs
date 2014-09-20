de836b22d9fb35b432c5a471f3312c41bfa579c8
Mohamed Tarek Ismail <blueice123@gmail.com>
2014-09-20 19:44:37 +0300

    Updated LocationComponents
    - Added ID to Zone and Room for comparison operators
    - Overloaded == and !=
    - Created constructors

--- Server/Server/LocationComponents.cs
+++ Server/Server/LocationComponents.cs
@@ -7,14 +7,24 @@ using Clients;
 
 namespace LocationComponents
 {
-    class Room
+    public class Room
     {
-        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; } 
         private List<Device> DeviceList = new List<Device>();
+
+        public Room(int _ID, string _Name)
+        {
+            ID = _ID;
+            Name = _Name;
+        }
 
         public void AddDevice(Device D)
         {
-            DeviceList.Add(D);
+            if (!DeviceList.Contains(D))
+                DeviceList.Add(D);
+            else
+                Console.WriteLine("Room already contains device: " + D.GetName());
         }
 
         public void RemoveDevice(Device D)
@@ -33,16 +43,60 @@ namespace LocationComponents
             foreach (Device D in DeviceList)
                 D.TurnOff();
         }
+
+        public static bool operator ==(Room R1, Room R2)
+        {
+            return R1.ID == R2.ID;
+        }
+
+        public static bool operator !=(Room R1, Room R2)
+        {
+            return R1.ID != R2.ID;
+        }
+
+        public override bool Equals(object o)
+        {
+            Room R = o as Room;
+            if (R != null)
+            {
+                return R == this;
+            }
+            return false;
+        }
+
+        public bool Equals(Room R)
+        {
+            if (R != null)
+            {
+                return R == this;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID;
+        }
     }
 
-    class Zone
+    public class Zone
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         private List<Room> RoomList = new List<Room>();
 
+        public Zone(int _ID, string _Name)
+        {
+            ID = _ID;
+            Name = _Name;
+        }
+
         public void AddRoom(Room R)
         {
-            RoomList.Add(R);
+            if (!RoomList.Contains(R))
+                RoomList.Add(R);
+            else
+                Console.WriteLine("Zone already contains room: " + R.Name);
         }
 
         public void RemoveRoom(Room R)
@@ -61,22 +115,64 @@ namespace LocationComponents
             foreach (Room R in RoomList)
                 R.TurnOffDevices();
         }
+
+        public static bool operator ==(Zone Z1, Zone Z2)
+        {
+            return Z1.ID == Z2.ID;
+        }
+
+        public static bool operator !=(Zone Z1, Zone Z2)
+        {
+            return Z1.ID != Z2.ID;
+        }
+
+        public override bool Equals(object o)
+        {
+            Zone Z = o as Zone;
+            if (Z != null)
+            {
+                return Z == this;
+            }
+            return false;
+        }
+
+        public bool Equals(Zone Z)
+        {
+            if (Z != null)
+            {
+                return Z == this;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID;
+        }
     }
 
-    class Home
+    public class Home
     {
     }
 
-    class Position
+    public class Position
     {
         public int xCoordinate { get; set; }
         public int yCoordinate { get; set; }
         public Room positionRoom;
 
+        public Position(int x, int y, Room R)
+        {
+            xCoordinate = x;
+            yCoordinate = y;
+            positionRoom = R;
+        }
+
         public Position(int x, int y)
         {
             xCoordinate = x;
             yCoordinate = y;
+            positionRoom = null;
         }
     }
 }
