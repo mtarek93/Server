16144fb31b98a1992fd984489cbf37289364c384
Mohamedbaha2ibrahim <mohamed_baha2@live.com>
2014-11-02 14:48:29 +0200

    dummy commit

--- TCP Device/TCP Device/Program.cs
+++ TCP Device/TCP Device/Program.cs
@@ -25,7 +25,7 @@ namespace TCP_Devices
                 Console.WriteLine("Connecting.....");
                 tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-                tcpSocket.Connect("10.7.162.125", 14);
+                tcpSocket.Connect("10.7.162.167", 14);
 
                 Console.WriteLine("Connected");
 
